34bca21b23bb17dfc1ca23745c66d0f4650df960
Sawmbawdy <saiompatro12@gmail.com>
2025-07-21 12:34:26 -0400

    Add files via upload

---
+++ FindingAlphabets.py
@@ -0,0 +1,7 @@
+Input = input("Type something random: \n")
+
+if str(Input) == True:
+    print("This is a String")
+    print("This is your Input:", Input)
+else:
+    print("This is not a string")
